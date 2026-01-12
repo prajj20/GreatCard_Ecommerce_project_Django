0135620d0c96b78ad48b58086dd74cb76424eb8d
prajakta <prajaktapawar072002@gmail.com>
2026-01-12 19:25:52 +0530

    created custome superadmin and users

---
+++ greatekart/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from store.models import Product
+def home(request):
+    product = Product.objects.all().filter(is_available=True)
+    context = {
+        'products':product
+    }
+    return render(request,'home.html',context)
